baa7a815c88b0dc2e3e24dd503be6c535fb16426
anabelen <anabelen@Mac-mini-de-borjaeguia.local>
2017-07-04 10:52:15 +0200

    j

--- proyecto1/plantilla.js
+++ proyecto1/plantilla.js
@@ -4,11 +4,12 @@
     app.controller('StoreController', function(){
         this.product = gem;
     });
-}) ();
 
-var gem = {
+    var gem = {
     name: 'Doce',
     price: 2.95,
     description: 'prueba description',
-    canPurchase: false
+    canPurchase: true,
 }
+}) ();
+
