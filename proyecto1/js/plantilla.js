e3c32a53917b7abc745c674d5dba71e786f4581d
Ana Belen <anabelengg6@gmail.com>
2017-07-04 11:13:48 +0200

    primera version

---
+++ proyecto1/js/plantilla.js
@@ -0,0 +1,15 @@
+(function(){
+    var app = angular.module('store', ['ngRoute']);
+
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+
+    var gem = {
+    name: 'Doce',
+    price: 2.95,
+    description: 'prueba description',
+    canPurchase: true,
+}
+}) ();
+
